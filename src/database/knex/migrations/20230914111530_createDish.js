9ad3f738d25d83874f266b4f0b2a605ebebdf4b5
André <andregparada@gmail.com>
2023-09-14 08:40:29 -0300

    criando migration no knex

---
+++ src/database/knex/migrations/20230914111530_createDish.js
@@ -0,0 +1,14 @@
+exports.up = knex => knex.schema.createTable("dishes", table => {
+    table.increments("id");
+    table.text("title");
+    table.text("image")
+    table.text("categorie");
+    table.text("description");
+    table.text("price");
+    table.integer("user_id").references("id").inTable("users");
+
+    table.timestamp("created_at").default(knex.fn.now())
+    table.timestamp("updated_at").default(knex.fn.now())
+});
+
+exports.down = knex => knex.schema.dropTable("dishes");
