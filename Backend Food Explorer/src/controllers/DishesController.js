826554438237919cd0fe0762b0fdd25711626df1
André <andregparada@gmail.com>
2023-10-03 13:19:13 -0300

    criado rota e controller de sessions

--- Backend Food Explorer/src/controllers/DishesController.js
+++ Backend Food Explorer/src/controllers/DishesController.js
@@ -26,7 +26,7 @@ class DishesController {
 
         await knex("ingredients").insert(ingredientsInsert);
 
-        response.json();
+        return response.json();
     }
 
     async show(request, response) {
--- Backend Food Explorer/src/routes/index.js
+++ Backend Food Explorer/src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 
 const usersRouter = require("./users.routes");
+const sessionsRouter = require("./sessions.routes");
 const dishesRouter = require("./dishes.routes");
 const ingredientsRouter = require("./ingredients.routes");
 
 const routes = Router();
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/dishes", dishesRouter);
 routes.use("/ingredients", ingredientsRouter);
 
