07ea42fe6c5a0a23a0b78bc46c48a3df57151b94
André <andregparada@gmail.com>
2023-10-09 13:15:48 -0300

    atualizando autenticação nos controllers e rotas

--- Backend Food Explorer/src/routes/dishes.routes.js
+++ Backend Food Explorer/src/routes/dishes.routes.js
@@ -1,14 +1,17 @@
 const { Router } = require("express");
 
 const DishesController = require("../controllers/DishesController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const dishesRoutes = Router();
 
 const dishesController = new DishesController();
 
-dishesRoutes.get("/", dishesController.index);
-dishesRoutes.post("/:user_id", dishesController.create);
+dishesRoutes.use(ensureAuthenticated);
+
+dishesRoutes.post("/", dishesController.create);
 dishesRoutes.get("/:id", dishesController.show);
 dishesRoutes.delete("/:id", dishesController.delete);
+dishesRoutes.get("/", dishesController.index);
 
 module.exports = dishesRoutes;
