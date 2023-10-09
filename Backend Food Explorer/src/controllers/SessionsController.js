b3051d2e88d93a5c344ee5b6889aecbe4e0687e8
André <andregparada@gmail.com>
2023-10-09 10:59:28 -0300

    criado sessions controller

--- Backend Food Explorer/src/controllers/SessionsController.js
+++ Backend Food Explorer/src/controllers/SessionsController.js
@@ -1,8 +1,25 @@
+const { use } = require("express/lib/router");
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compar, compare } = require("bcryptjs");
+
 class SessionsController {
     async create(request, response) {
-        const { email, password } = request.body
+        const { email, password } = request.body;
 
-        return response.json({ email, password })
+        const user = await knex("users").where({ email }).first();
+
+        if(!user) {
+            throw new AppError("E-mail e/ou senha incorreta.", 401);
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched) {
+            throw new AppError("E-mail e/ou senha incorreta.", 401);
+        }
+
+        return response.json(user);
     }
 }
 
