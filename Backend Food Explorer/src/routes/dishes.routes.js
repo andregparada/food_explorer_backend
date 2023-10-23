9251318b41fa36141a2c4bda30f58325769e2423
André <andregparada@gmail.com>
2023-10-23 09:37:50 -0300

    salvar imagem

--- Backend Food Explorer/src/routes/dishes.routes.js
+++ Backend Food Explorer/src/routes/dishes.routes.js
@@ -1,17 +1,25 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const DishesController = require("../controllers/DishesController");
+const DishImageController = require("../controllers/DishImageController")
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization")
 
 const dishesRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
+
 
 const dishesController = new DishesController();
+const dishImageController = new DishImageController();
 
 dishesRoutes.use(ensureAuthenticated);
 
-dishesRoutes.post("/", dishesController.create);
+dishesRoutes.post("/", verifyUserAuthorization("admin"), dishesController.create);
 dishesRoutes.get("/:id", dishesController.show);
-dishesRoutes.delete("/:id", dishesController.delete);
+dishesRoutes.delete("/:id", verifyUserAuthorization("admin"), dishesController.delete);
 dishesRoutes.get("/", dishesController.index);
+dishesRoutes.patch("/:id", ensureAuthenticated, upload.single("image"), dishImageController.update);
 
 module.exports = dishesRoutes;
